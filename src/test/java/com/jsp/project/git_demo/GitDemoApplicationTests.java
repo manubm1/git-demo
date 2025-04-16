88e20a50ec77dff673895882e7640df69a9f895c
manojbm12 <manojbmmanu77@gmail.com>
2025-04-16 15:08:43 +0530

    first commit | project intiated

---
+++ src/test/java/com/jsp/project/git_demo/GitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.project.git_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
